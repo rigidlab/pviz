f68e8db7c8c957b74ac6343366d3818b10d8ed2f
phu <pta@fetchrobotics.com>
2018-06-19 08:41:25 -0700

    Adding timestamp axis for state class

--- pviz.py
+++ pviz.py
@@ -1,7 +1,11 @@
+import math
+import numpy as np
+import pandas as pd
 from bokeh.plotting import figure, output_file, show, save
 from bokeh.models import HoverTool, CustomJS, ColumnDataSource,Slider
 from bokeh.models import Arrow, NormalHead
 from bokeh.layouts import column,layout,row
+from bokeh.models.formatters import DatetimeTickFormatter
 
 class space():
     """
@@ -12,8 +16,8 @@ class space():
         tools=["pan,box_zoom,wheel_zoom,save,reset"]
         self.p = figure(plot_width=w,plot_height=h,logo=None,
                 aspect_scale=1,match_aspect=True,tools=tools,**kwargs)
-        self.p.xgrid.grid_line_color=None
-        self.p.ygrid.grid_line_color=None
+        #self.p.xgrid.grid_line_color=None
+        #self.p.ygrid.grid_line_color=None
         self.source=source
 
     def add_circle(self,x,y,**kwargs):
@@ -23,7 +27,9 @@ class space():
         self.p.add_layout(Arrow(end=NormalHead(size=5), line_color="red",
                                 x_start='x',y_start='y',x_end='x+0.8',y_end='y+0.6',
                                 source=self.source))
-    
+    def add_line(self,**kwargs):
+        self.p.line(source=self.source,**kwargs)
+
     def add_rectangle(self,x,y,w=1,h=1):
         self.p.rect(x,y,w,h)
 
@@ -40,13 +46,40 @@ class state():
     """
     Figure class that plot state variables vs time
     """
-    def __init__(self,w=500,h=500,**kwargs):
+    def __init__(self,source,w=500,h=500,**kwargs):
         hover=HoverTool(tooltips=[("x,y","$x,$y")])
-        tools=["pan,box_zoom,wheel_zoom,save,reset,crosshair",hover]
-        self.p = figure(plot_width=w,plot_height=h,**kwargs,logo=None,
-                aspect_scale=1,match_aspect=True,tools=tools)
-        self.p.xgrid.grid_line_color=None
-        self.p.ygrid.grid_line_color=None
+        tools=["pan,box_zoom,wheel_zoom,save,reset"]
+        self.p = figure(plot_width=w,plot_height=h,logo=None,
+                aspect_scale=1,match_aspect=True,tools=tools,**kwargs)
+        #self.p.xgrid.grid_line_color=None
+        #self.p.ygrid.grid_line_color=None
+        self.p.xaxis.formatter=DatetimeTickFormatter(
+            minsec=["%m/%d/%Y %H:%M:%S"],
+            minutes=["%m/%d/%Y %H:%M:%S"],
+            hourmin=["%m/%d/%Y %H:%M:%S"],
+            seconds=["%m/%d/%Y %H:%M:%S"],
+            hours=["%m/%d/%Y %H:%M:%S"],
+            days=["%m/%d/%Y %H:%M:%S"],
+            months=["%m/%d/%Y %H:%M:%S"],
+            years=["%m/%d/%Y %H:%M:%S"],
+        )   
+        self.p.xaxis.major_label_orientation = math.pi/4
+        self.source=source
+
+    def add_vector(self):    
+        self.p.add_layout(Arrow(end=NormalHead(size=5), line_color="red",
+                                x_start='x',y_start='y',x_end='x+0.8',y_end='y+0.6',
+                                source=self.source))
+    def add_line(self,t,state,color=None):
+        self.p.line(x=t,y=state,source=self.source,legend=dict(value=state),
+            line_color=color)
+
+    def add_circle(self,t,state,color=None):
+        self.p.circle(x=t,y=state,source=self.source,legend=dict(value=state),
+            line_color=color,fill_color=color,)
+
+    def add_rectangle(self,x,y,w=1,h=1):
+        self.p.rect(x,y,w,h)
 
     def plot(self,name="pviz.html",pshow=False):
         output_file(name)
@@ -58,7 +91,19 @@ class state():
         return self.p
 
 if __name__=="__main__":
-    p = space(x_range=(-5,5),y_range=(-5,5))
-    p.add_circle(2,2,radius=0.5)
-    p.add_rectangle(1,1)
-    p.plot()
+    df=pd.read_csv("fmcl.csv",header=0)
+    df['t']=pd.to_datetime(df['t'],unit='s')
+    print (df.head())
+    p = space(df)
+    #p.add_circle(2,2,radius=0.5)
+    #p.add_rectangle(1,1)
+    p.add_line(x='x',y='y')
+    #p.plot()
+     
+    ymin,ymax = (df.loc[:, ['x', 'y']].min().min(),df.loc[:, ['x', 'y']].max().max())
+    delta=ymax-ymin
+    p2 = state(df,y_range=(ymin-delta/2,ymax+delta/2))
+    p2.add_circle('t','y',color='red')
+    p2.add_circle('t','x',color='green')
+    show(column(p.get_figure(),p2.get_figure()))
+    #show(p2.get_figure())
